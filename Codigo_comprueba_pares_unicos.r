e367c8a02d8174bf8b92d69f0d483dec60d02062
Pedro Ferrer Breda <123893612+pedroferrerbreda@users.noreply.github.com>
2024-06-18 12:06:24 -0600

    Update Codigo_comprueba_pares_unicos.r

--- Codigo_comprueba_pares_unicos.r
+++ Codigo_comprueba_pares_unicos.r
@@ -1,3 +1,5 @@
+# primero de todo guardar el codigo modificado y reiniciar R para no asegurarse de que el RAM no esta ocupado
+
 #crear llave pares
 df$llave <- paste(bloqueo_ejemplo$id1,'_',bloqueo_ejemplo$id2, sep="")
  
@@ -14,5 +16,12 @@ df %>% distinct(id1, id2, .keep_all = TRUE)
 max(bloqueo_ejemplo$VER)
 min(bloqueo_ejemplo$VER)
 
+# si se borran muchos, comprueba si se puede correr la tabla sin acabar con el RAM
+ # si no comprueba los valores menores/iguales a 1 y avisa a Ivan
+
 #filtrar pares con similitud menor o igual a 1
 comprueba <- bloqueo_ejemplo[which(bloqueo_ejemplo$VER<=1),]
+
+# filtrar pares con similitud mayor que 1
+comprueba2 <- bloqueo_ejemplo[which(bloqueo_ejemplo$VER>1),]
+
